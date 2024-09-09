d4a099916945e8f09e4f1407b5f1e82fa4310ad3
Mustafashairali <mustafashairali804@gmail.com>
2024-09-09 20:53:53 +0500

    first commit

---
+++ java/script.js
@@ -0,0 +1,6 @@
+function print() {
+    var username = document.getElementById('name').value
+    var password = document.getElementById('password').value
+    console.log('Username:', username)
+    console.log('Password:', password)
+}
